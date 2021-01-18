fe7512c74a6d527301867620d25d3c2f02cf01c1
SaurabhBagde <bagdesaurabh69@gmail.com>
2021-01-18 09:27:54 +0530

    removed portfolio thickbox bug

--- template-parts/content-df-portfolio.php
+++ template-parts/content-df-portfolio.php
@@ -9,24 +9,24 @@
 
 	<div id="portfolio-item-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php 
-		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
-			return;
-		}
+	<?php
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
 
-		if ( is_singular( 'df-portfolio' ) ) :
-			?>
+	if ( is_singular( 'df-portfolio' ) ) :
+		?>
 			
 			<header class="entry-header">
 				<div class="entry-title">
-					<?php the_title(); ?>
+				<?php the_title(); ?>
 				</div>
 
 				<div class="entry-meta">
-				<?php
-					dsign_fly_posted_by();
-					dsign_fly_posted_on();
-				?>
+			<?php
+				dsign_fly_posted_by();
+				dsign_fly_posted_on();
+			?>
 
 				<span class="comments"><?php echo get_comments_number(); ?> Comment(s) </span> 
 			</div><!-- .entry-meta -->
@@ -43,28 +43,32 @@
 				<?php the_content(); ?>
 			</div>
 			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dsign-fly' ),
-				'after'  => '</div>',
-			) );
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dsign-fly' ),
+					'after'  => '</div>',
+				)
+			);
 			?>
-		<?php else: ?>
-			<?php if(!is_front_page(  )) :?>
-			<!-- <div class="view-image">
-				
-				<span class="dashicons dashicons-instagram"></span>
-				<span>View Image</span>
-			</div> -->
-			<a class="post-thumbnail thickbox" rel="lightbox" href="<?php echo get_the_post_thumbnail_url() . '?TB_iframe=true&width=600&height=550'//the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php the_post_thumbnail(); ?>
+		<?php else : ?>
+			<?php if ( ! is_front_page() ) : ?>
+			<a class="post-thumbnail thickbox" rel="lightbox" href="<?php echo get_the_post_thumbnail_url() . '?TB_iframe=true&width=600&height=550';// the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+				<div class="view-image">
+					
+					<span class="dashicons dashicons-instagram"></span>
+					<span>View Image</span>
+				</div>
 			</a>
+			<!-- <a class="post-thumbnail thickbox" rel="lightbox" href="<?php // echo get_the_post_thumbnail_url() . '?TB_iframe=true&width=600&height=550'//the_permalink(); ?>" aria-hidden="true" tabindex="-1"> -->
+				<?php the_post_thumbnail(); ?>
+			<!-- </a> -->
 			
 			<div class="post-title">
 				<a aia-hidden="true" href="<?php the_permalink(); ?>" tabindex="-1">
 					<?php the_title(); ?>
 				</a>
 			</div>
-			<?php else:?>
+			<?php else : ?>
 
 				<div class="view-image">
 				
@@ -74,9 +78,9 @@
 			<a class="post-thumbnail " href="<?php echo the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 				<?php the_post_thumbnail(); ?>
 			</a>
-		<?php
+				<?php
 		endif;
 		endif; // End is_singular().
-	?>
+		?>
 
-    </div><!-- #post-<?php the_ID(); ?> -->
+	</div><!-- #post-<?php the_ID(); ?> -->
